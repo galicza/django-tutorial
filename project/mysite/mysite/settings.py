02ad0fe5caab3d5bcd3e36390595784101b286ec
galicza <galicza.zsuzsa@gmail.com>
2015-03-11 16:59:36 +0100

    'First'

---
+++ project/mysite/mysite/settings.py
@@ -0,0 +1,90 @@
+"""
+Django settings for mysite project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import pprint
+import sys
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates/admin/')]
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '(xpwiy$mf__#$bps%(+0k$e1o8%uymcnvrkz4(p&*$6une$oif'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'polls',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'mysite.urls'
+
+WSGI_APPLICATION = 'mysite.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Europe/Budapest'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
---
+++ vagrant_setup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+exvagrant 
+echo 'Updating...'
+sudo apt-get update
+
+echo 'Installing Apache'
+sudo apt-get install -y apache2
+
+echo 'Installing mod_wsgi'
+sudo apt-get install -y libapache2-mod-wsgi
+#AddHandler cgi-script .cgi .pl .python-pip
+#sudo service apache2 restart
+
+echo 'Installing MySQL'
+sudo debconf-set-selections <<< 'mysql-server \
+ mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server \
+ mysql-server/root_password_again password root'
+
+sudo apt-get install -y mysql-server
+# During the installation process you will be prompted to enter a password for the MySQL root user.
+sudo apt-get install -y mysql-client
+
+echo 'Installing pip'
+sudo apt-get install -y python-pip
+
+echo 'Installing Django'
+sudo pip install Django
+
+echo 'Installing MySQL-python'
+sudo apt-get install -y python-mysqldb
+
+echo 'Installing git'
+sudo apt-get install -y git
+
+echo 'Setting env var for Django settings'
+echo "export DJANGO_SETTINGS_MODULE=mysite.settings" >> ~/.profilevagran
+
+
