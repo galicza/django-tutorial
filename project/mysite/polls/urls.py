31ee4348bd6f6014984697197768341b5561788a
galicza <galicza.zsuzsa@gmail.com>
2015-03-13 14:10:20 +0100

    Templates part done

--- project/mysite/polls/urls.py
+++ project/mysite/polls/urls.py
@@ -4,7 +4,7 @@ from polls import views
 urlpatterns = patterns('',
                        # ex: /polls/
                        url(r'^$', views.index, name='index'),
-                       # ex: /polls/5/
+                       # the 'name' value as called by the {% url %} template tag | ex: /polls/5/
                        url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
                        # ex: /polls/5/results/
                        url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
